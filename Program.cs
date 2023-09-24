e35f858220c45ef60f570f6b428d0a61cae993cf
«Oleg» <Pan159dom159@yandex.ru>
2023-09-24 21:40:52 +0300

    Написал программу которая формирует массив,где длинна строк меньше или равно 3

--- Program.cs
+++ Program.cs
@@ -3,5 +3,26 @@
 //       либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
 //       лучше обойтись исключительно массивами.
 
+Console.Clear();
 
+string[] array = { "Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan","4","pip897","arr","Array" };
 
+string[] SortArray(string[] array)
+{
+    string[] newArray = new string[array.Length];
+
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+    return newArray;
+}
+
+Console.Write($"Начальный массив :[" + string.Join(", ", array) + "]"+"\n"+"\n");
+Console.Write($"Конечный массив :[" + string.Join(", ", SortArray(array)) + "]");
